8f1afc3c6a4b08c2c0411f4da8d2f596ce710cdb
in3orn <in3orn@gmail.com>
2019-08-01 14:44:11 +0200

    Show / Hide photos panel

--- Assets/Scripts/Photos/PhotosMediator.cs
+++ Assets/Scripts/Photos/PhotosMediator.cs
@@ -16,17 +16,32 @@ namespace Photos
 
         private void Start()
         {
+            photosView.Hide();
             photosView.Refresh(photosController.Photos);
         }
 
         private void OnEnable()
         {
+            photosView.showButton.onClick.AddListener(HandleShowButtonClicked);
+            photosView.hideButton.onClick.AddListener(HandleHideButtonClicked);
             photosController.OnUpdated.AddListener(HandlePhotosUpdated);
         }
 
         private void OnDisable()
         {
+            photosView.showButton.onClick.RemoveListener(HandleShowButtonClicked);
+            photosView.hideButton.onClick.RemoveListener(HandleHideButtonClicked);
             photosController.OnUpdated.RemoveListener(HandlePhotosUpdated);
+        }
+
+        private void HandleShowButtonClicked()
+        {
+            photosView.Show();
+        }
+
+        private void HandleHideButtonClicked()
+        {
+            photosView.Hide();
         }
 
         private void HandlePhotosUpdated()
--- Assets/Scripts/Photos/PhotosView.cs
+++ Assets/Scripts/Photos/PhotosView.cs
@@ -1,14 +1,19 @@
 using System.Collections.Generic;
 using Common;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 namespace Photos
 {
     public class PhotosView : MonoBehaviour
     {
+        public Button showButton;
+        public Button hideButton;
+
         [SerializeField] private RectTransform content;
         [SerializeField] private PhotoView template;
+        [SerializeField] private GameObject panel;
 
         [Inject] private DynamicContent dynamicContent;
 
@@ -17,6 +22,16 @@ namespace Photos
         public PhotosView()
         {
             photoViews = new List<PhotoView>();
+        }
+
+        public void Show()
+        {
+            panel.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            panel.SetActive(false);
         }
 
         public void Refresh(IList<PhotoData> photos)
